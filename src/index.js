846ef126e12b0882febd0a75eda9344ed8fa87f7
mathis <mathissineux@gmail.com>
2023-05-19 21:09:38 +0200

    initial config firebas at index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-console.log ('hello world test')
+import { initializeApp } from 'firebase/app'
 
+const firebaseConfig = {
+    apiKey: "AIzaSyABQI6705CXK7mWbrRB-Me9qCMfJjFQhsY",
+    authDomain: "mindgeek-60f9a.firebaseapp.com",
+    projectId: "mindgeek-60f9a",
+    storageBucket: "mindgeek-60f9a.appspot.com",
+    messagingSenderId: "111523513658",
+    appId: "1:111523513658:web:d0014c563b79d042c3fc03",
+    measurementId: "G-FWRCQB4FMR"
+  };
 
-
-
-// import { initializeApp } from 'firebase/app'
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyABQI6705CXK7mWbrRB-Me9qCMfJjFQhsY",
-//   authDomain: "mindgeek-60f9a.firebaseapp.com",
-//   projectId: "mindgeek-60f9a",
-//   storageBucket: "mindgeek-60f9a.appspot.com",
-//   messagingSenderId: "111523513658",
-//   appId: "1:111523513658:web:d0014c563b79d042c3fc03",
-//   measurementId: "G-FWRCQB4FMR"
-// };
-
-// initializeApp(firebaseConfig)
+  initializeApp(firebaseConfig)
